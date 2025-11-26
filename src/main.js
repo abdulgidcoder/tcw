df179f654c11ec65c5de66aadaaa38f8ecfd2c3e
abdulgidcoder <abdulgidcoder@gmail.com>
2025-11-27 00:15:43 +0200

    feat: enable app loader and update image assets

--- src/main.js
+++ src/main.js
@@ -6,114 +6,68 @@ import { Navigation, Pagination, Autoplay, Thumbs, EffectFade } from "swiper/mod
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import 'swiper/css/effect-fade';
-import 'bootstrap/dist/css/bootstrap.rtl.min.css';
+import "swiper/css/effect-fade";
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
 import "@/assets/scss/app.scss";
 
+document.addEventListener("DOMContentLoaded", () => {
+  const loader = document.getElementById("app-loader");
+  if (loader) setTimeout(() => (loader.style.display = "none"), 800);
+  // Slideshow
+  const slideshow = new Swiper(".slideshow-section .swiper", {
+    slidesPerView: 1,
+    loop: true,
+    effect: "fade",
+    fadeEffect: { crossFade: true },
+    autoplay: { delay: 3000, disableOnInteraction: false },
+    navigation: {
+      nextEl: ".slideshow-section .swiper-button-next",
+      prevEl: ".slideshow-section .swiper-button-prev",
+    },
+    pagination: {
+      el: ".slideshow-section .swiper-pagination",
+      dynamicBullets: true,
+    },
+    modules: [Navigation, Autoplay, EffectFade, Pagination],
+  });
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   setTimeout(() => {
-//     document.getElementById("app-loader").style.display = "none";
-//   }, 800); 
-// });
-// init Swiper:
-const slideshow = new Swiper(".slideshow-section .swiper", {
-  slidesPerView: 1,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
-  effect: "fade",
-  spaceBetween: 30,
-  fadeEffect: { crossFade: true },
-  loop: true,
-  navigation: {
-    nextEl: ".slideshow-section .swiper-button-next",
-    prevEl: ".slideshow-section .swiper-button-prev",
-  },
-  pagination: {
-    el: ".slideshow-section .swiper-pagination",
-    dynamicBullets: true,
-  },
-  modules: [Navigation, Autoplay, EffectFade, Pagination],
-}); 
-const coursesCates = new Swiper(".courses-carousel .swiper", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  breakpoints: {
-    768: {
-      spaceBetween: 20,
-      slidesPerView: 2,
+  // Courses carousel
+  const coursesCates = new Swiper(".courses-carousel .swiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      768: { slidesPerView: 2, spaceBetween: 20 },
+      992: { slidesPerView: 3, spaceBetween: 25 },
+      1400: { slidesPerView: 4 },
     },
-    992: {
-      spaceBetween: 25,
-      slidesPerView: 3,
+    navigation: {
+      nextEl: ".courses-carousel .swiper-button-next",
+      prevEl: ".courses-carousel .swiper-button-prev",
     },
-    1400: {
-      slidesPerView: 4,
-    },
-  },
-  navigation: {
-    nextEl: ".courses-carousel .swiper-button-next",
-    prevEl: ".courses-carousel .swiper-button-prev",
-  },
-  modules: [Navigation],
-});
+    modules: [Navigation],
+  });
 
-const partners = new Swiper(".partners-section .swiper", {
-  slidesPerView: 2, 
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
+  // Partners carousel
+  const partners = new Swiper(".partners-section .swiper", {
+    slidesPerView: 2,
+    breakpoints: {
+      768: { slidesPerView: 3 },
+      992: { slidesPerView: 5 },
+      1400: { slidesPerView: 6 },
     },
-    992: {
-      slidesPerView: 4,
+    navigation: {
+      nextEl: ".partners-section .swiper-button-next",
+      prevEl: ".partners-section .swiper-button-prev",
     },
-    992: {
-      slidesPerView: 5, 
+    pagination: {
+      el: ".partners-section .swiper-pagination",
+      clickable: true,
     },
-    1400: {
-      slidesPerView: 6,
-    },
-  },
-  navigation: {
-    nextEl: ".partners-section .swiper-button-next",
-    prevEl: ".partners-section .swiper-button-prev",
-  },
-  pagination: {
-    el: ".partners-section .swiper-pagination",
-    clickable: true,
-  },
-  modules: [Navigation, Pagination],
+    modules: [Navigation, Pagination],
+  });
 });
 
-const projectThumbs = new Swiper(".slideshow-project-thumbs", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-  breakpoints: {
-    992: {
-      slidesPerView: 5,
-    },
-    1400: {
-      slidesPerView: 6,
-    },
-  },
-});
-const slideshowProject = new Swiper(".slideshow-project .swiper", {
-  loop: true,
-  spaceBetween: 10,
-  autoplay: {
-    delay: 2500,
-  },
-  navigation: {
-    nextEl: ".slideshow-project .swiper-button-next",
-    prevEl: ".slideshow-project .swiper-button-prev",
-  },
-  modules: [Navigation, Autoplay, Thumbs],
-  thumbs: {
-    swiper: projectThumbs,
-  },
-});
+
+
+
 
